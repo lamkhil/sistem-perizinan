b47de86ec0288338bf6686759415b0826d56bfc0
MUlilAMrI23091397091 <149356238+MUlilAMrI23091397091@users.noreply.github.com>
2025-10-15 10:07:25 +0700

    Remove gap between table and TTD - TTD now starts directly after last data row

--- app/Exports/PenerbitanBerkasExport.php
+++ app/Exports/PenerbitanBerkasExport.php
@@ -189,7 +189,7 @@ class PenerbitanBerkasExport implements FromCollection, WithHeadings, WithMappin
                 }
                 
                 // Add TTD section after data - POSITIONED UNDER SPECIFIC COLUMNS
-                $ttdRow = $lastRow + 1;
+                $ttdRow = $lastRow;
                 
                 // TTD Mengetahui (kiri) - di bawah kolom TANGGAL PERMOHONAN (kolom D)
                 $sheet->setCellValue('D' . $ttdRow, 'Mengetahui');
